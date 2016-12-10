ab28aa36fc7c82569122471e1fc87f5e0bf4558b
JGUANILO <JGUANILO@JGUANILO>
2016-12-09 19:47:00 -0500

    Jguanilo

---
+++ 1 Interface Layer/Angkor.O7Web.Interface.SharedSource/Controllers/ErrorController.cs
@@ -0,0 +1,22 @@
+﻿using System.Web.Mvc;
+
+namespace Angkor.O7Web.Interface.SharedSource.Controllers
+{
+    public class ErrorController : Controller
+    {
+        public ActionResult Error401()
+        {
+            return View();
+        }
+
+        public ActionResult Error404()
+        {
+            return View();
+        }
+
+        public ActionResult Error500()
+        {
+            return View();
+        }
+    }
+}
--- 1 Interface Layer/Angkor.O7Web.Interface.SharedSource/Global.asax.cs
+++ 1 Interface Layer/Angkor.O7Web.Interface.SharedSource/Global.asax.cs
@@ -1,9 +1,21 @@
-﻿namespace Angkor.O7Web.Interface.SharedSource
+﻿using System.Web.Mvc;
+using System.Web.Routing;
+using Angkor.O7Web.Interface.SharedSource.Controllers;
+
+namespace Angkor.O7Web.Interface.SharedSource
 {
     public class MvcApplication : System.Web.HttpApplication
     {
         protected void Application_Start()
-        {                        
+        {
+            RouteTable.Routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            RouteTable.Routes.MapRoute(
+                name: "Default",
+                url: "{controller}/{action}/{id}",
+                defaults: new { controller = "Error", action = "Error401", id = UrlParameter.Optional },
+                namespaces: new[] { typeof(ErrorController).Namespace }
+            );
         }
     }
 }
