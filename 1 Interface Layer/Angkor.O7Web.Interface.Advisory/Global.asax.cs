47e20ded071289ec868e8f9185f721d6a32d6e87
felix_dev <felix_dev@DESKTOP-52OMMRE>
2017-01-17 03:43:21 -0500

    Update web projects

--- 1 Interface Layer/Angkor.O7Web.Interface.Advisory/Global.asax.cs
+++ 1 Interface Layer/Angkor.O7Web.Interface.Advisory/Global.asax.cs
@@ -1,10 +1,6 @@
-﻿using System;
-using System.Web;
+﻿using System.Web;
 using System.Web.Mvc;
 using System.Web.Routing;
-using System.Web.Security;
-using Angkor.O7Framework.Web.Security;
-using Angkor.O7Web.Interface.AppStart;
 using Angkor.O7Web.Interface.AppStart.AppStart;
 
 namespace Angkor.O7Web.Interface.Advisory
